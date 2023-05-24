004035e8cf4d4aacfaf051f500e79b88e185a1b9
Vladyslav Yarema <vlad14yarema@gmail.com>
2023-05-24 21:13:42 +0300

    added some changes to the project

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ def create_app():
     client = MongoClient(os.getenv("MONGO_URI"))
     app.db = client.microblog
 
+    habits = ['Test habit', 'Test habit 2']
+
     @app.route('/', methods=["GET", "POST"])
     def home():
         if request.method == "POST":
@@ -30,9 +32,40 @@ def create_app():
 
         return render_template("home.html", entries=reversed(entries_with_date))
 
+    @app.context_processor
+    def add_calc_date_range():
+        def date_range(start: datetime.date):
+            dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+            return dates
+
+        return {"date_range": date_range}
+
+    @app.route("/habit/")
+    def habit():
+        date_str = request.args.get("date")
+        if date_str:
+            selected_date = datetime.date.fromisoformat(date_str)
+        else:
+            selected_date = datetime.date.today()
+        return render_template(
+            "habit.html",
+            habits=habits,
+            title="Habit Tracker",
+            selected_date=selected_date
+            )
+
+    @app.route("/habit/add/", methods=["GET", "POST"])
+    def add_habit():
+        if request.method == "POST":
+            habits.append(request.form.get("habit"))
+        return render_template(
+            "add_habit.html",
+            title="Habit Tracker - Add Habit",
+            selected_date=datetime.date.today()
+        )
+
     return app
 
 
 # if __name__ == '__main__':
 #     create_app().run(debug=True)
-
