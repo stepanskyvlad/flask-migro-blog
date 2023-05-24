9f201a7b496e65eab115d5cccd1df2ed7b39889c
Vladyslav Yarema <vlad14yarema@gmail.com>
2023-05-24 11:59:26 +0300

    edited app.py

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template
+import datetime
+import os
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+load_dotenv()
 
 
-@app.route('/')
-def home():
-    return render_template("home.html")
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.getenv("MONGO_URI"))
+    app.db = client.microblog
+
+    @app.route('/', methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
+
+        entries_with_date = [
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d"),
+            )
+            for entry in app.db.entries.find({})
+        ]
+
+        return render_template("home.html", entries=reversed(entries_with_date))
+
+    return app
 
 
 # if __name__ == '__main__':
-#     app.run()
+#     create_app().run(debug=True)
+
